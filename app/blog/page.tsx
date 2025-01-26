7b85e71ccf41b0d1100dab41fe50abc3e8eb6458
Edwin Chang <edwinchang2024@gmail.com>
2025-01-26 14:48:33 -0800

    add blog posts to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,17 @@
-export default function Blog() {
+import { promises as fs } from "fs";
+import Link from "next/link";
+
+export default async function Blog() {
+    const posts: { title: string; date: string; path: string }[] = JSON.parse(
+        await fs.readFile(process.cwd() + "/app/blog/posts.json", "utf8"),
+    );
     return (
-        <div className="w-1/2 mx-auto pt-8 space-y-8">
-            <div>
-                <div className="text-xl">mm/dd/yyyy: some blog post</div>
-                <div>description</div>
-            </div>
-            <div>
-                <div className="text-xl">mm/dd/yyyy: another blog post</div>
-                <div>description</div>
-            </div>
+        <div className="w-1/2 mx-auto pt-8 flex flex-col space-y-8">
+            {posts.map((post) => (
+                <Link href={"/blog" + post.path} key={post.path} className="text-xl">
+                    {post.date}: {post.title}
+                </Link>
+            ))}
         </div>
     );
 }
