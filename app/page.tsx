89f21434dd41f84d9e621b740a4ed6846d3a7fe0
rchang0 <64290256+rchang0@users.noreply.github.com>
2025-01-25 19:12:18 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
         <div className="space-y-8 p-8">
             <div className="text-center text-3xl">becca chang</div>
             <div className="text-center w-1/2 mx-auto">
-                I&aposm a third year undergrad at MIT studying Physics and Electrical Engineering (8 and 6-1). I&aposm currently doing research in Prof. Isaac Chuang&aposs lab, and I&aposm widely interested in quantum tech and stat mech. At MIT, I&aposm most involved in Puzzle Club, Wind Ensemble, and iQuHACK.
+                I&apos;m a third year undergrad at MIT studying Physics and Electrical Engineering (8 and 6-1). I&apos;m currently doing research in Prof. Isaac Chuang&apos;s lab, and I&apos;m widely interested in quantum tech and stat mech. At MIT, I&apos;m most involved in Puzzle Club, Wind Ensemble, and iQuHACK.
             </div>
         </div>
     );
