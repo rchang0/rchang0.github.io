5ab2e8d88916073bd3efcfe7f781170a3b44eeab
Edwin Chang <edwinchang2024@gmail.com>
2025-01-25 16:58:20 -0800

    add blog page skeleton and update navbar

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,14 @@
 export default function Blog() {
-    return <div>blog</div>;
+    return (
+        <div className="w-1/2 mx-auto pt-8 space-y-8">
+            <div>
+                <div className="text-xl">mm/dd/yyyy: some blog post</div>
+                <div>description</div>
+            </div>
+            <div>
+                <div className="text-xl">mm/dd/yyyy: another blog post</div>
+                <div>description</div>
+            </div>
+        </div>
+    );
 }
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,11 +1,29 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
+    const pathname = usePathname();
+    const entries: {
+        name: string;
+        href: string;
+    }[] = [
+        { name: "home", href: "/" },
+        { name: "puzzles", href: "/puzzles" },
+        { name: "blog", href: "/blog" },
+    ];
     return (
         <div className="flex flex-row justify-center space-x-8 text-xl p-4">
-            <Link href="/">home</Link>
-            <Link href="/puzzles">puzzles</Link>
-            <Link href="/blog">blog</Link>
+            {entries.map((entry) => (
+                <Link
+                    key={entry.href}
+                    href={entry.href}
+                    className={(pathname == entry.href ? "font-bold " : "") + "transition-[font-weight]"}
+                >
+                    {entry.name}
+                </Link>
+            ))}
         </div>
     );
 }
