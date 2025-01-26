92a71ec56f832ce11e350f45fed4c39112bc4ca3
rchang0 <64290256+rchang0@users.noreply.github.com>
2025-01-25 19:36:44 -0500

    Update page.tsx

--- app/puzzles/page.tsx
+++ app/puzzles/page.tsx
@@ -1,3 +1,6 @@
 export default function Puzzles() {
-    return <div>under construction... check back later</div>;
+    return <div>
+        under construction... check back later <br />
+        (this is not a puzzle)
+    </div>;
 }
