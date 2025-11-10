d61d4d8496b6787740f715401c1fe0ef9e13f516
rchang0 <64290256+rchang0@users.noreply.github.com>
2025-11-10 14:57:13 -0500

    Update bio

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
             <h1 className="text-3xl text-center">becca chang</h1>
             <p className="text-base my-2 text-center">rebchang[at]mit[dot]edu</p>
             <p className="text-center my-4">
-                I&apos;m an undergrad and incoming MEng at MIT studying Physics and Electrical Engineering (8 and 6-1), and minoring in
-                Music (21M). I&apos;m currently doing quantum research at Los Alamos National Lab, and for the past year I was in Prof. Isaac Chuang&apos;s lab working on quantum sensing and quantum signal processing.
-                I&apos;m widely interested in quantum info and stat mech. At MIT, I&apos;m most involved in Puzzle Club,
+                I&apos;m a dual status undergrad and MEng at MIT studying Physics and Electrical Engineering (8 and 6-1), and minoring in
+                Music (21M). I&apos;m currently doing my thesis research co-advised by Jordan Cotler and Anand Natarajan, and in the past I&apos;ve worked at Los Alamos National Lab and in Isaac Chuang&apos;s lab.
+                I&apos;m widely interested in quantum info theory, particularly quantum algorithms and quantum learning, and stat mech. At MIT, I&apos;m most involved in Puzzle Club,
                 Wind Ensemble, and iQuHACK.
             </p>
         </div>
