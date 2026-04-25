7ff0335060efd3d3646ca32f49387fe0b18ec77a
rchang0 <64290256+rchang0@users.noreply.github.com>
2026-04-24 20:37:26 -0400

    Update navbar.tsx

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,26 +2,32 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function NavBar() {
     const pathname = usePathname();
-    const entries: {
-        name: string;
-        href: string;
-    }[] = [
+    const [hoveredHref, setHoveredHref] = useState<string | null>(null);
+
+    const entries: { name: string; href: string }[] = [
         { name: "home", href: "/" },
         { name: "puzzles", href: "/puzzles" },
         { name: "blog", href: "/blog" },
     ];
+
     return (
         <div className="flex flex-row justify-center space-x-8 text-xl p-4">
             {entries.map((entry) => (
                 <Link
                     key={entry.href}
                     href={entry.href}
-                    className={(pathname == entry.href ? "font-bold " : "") + "transition-[font-weight]"}
+                    onMouseEnter={() => setHoveredHref(entry.href)}
+                    onMouseLeave={() => setHoveredHref(null)}
+                    className={
+                        (pathname === entry.href ? "font-bold " : "") +
+                        "transition-all"
+                    }
                 >
-                    {entry.name}
+                    {hoveredHref === entry.href ? `|${entry.name}>` : entry.name}
                 </Link>
             ))}
         </div>
