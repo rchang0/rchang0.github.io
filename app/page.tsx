2483a53ccee5fc29f7b802792b6e04be4b237d79
rchang0 <64290256+rchang0@users.noreply.github.com>
2026-06-23 13:59:06 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <p className="text-base my-2 text-center">rebchang[at]mit[dot]edu</p>
             <p className="text-center my-4">
                 I&apos;m a dual status undergrad and MEng at MIT studying Physics and Electrical Engineering (8 and 6-1), and minoring in
-                Music (21M). I&apos;m currently doing my thesis research co-advised by Jordan Cotler and Anand Natarajan, and in the past I&apos;ve worked at Los Alamos National Lab and in Isaac Chuang&apos;s lab.
+                Music (21M). I did my thesis research co-advised by Jordan Cotler and Anand Natarajan, and in the past I&apos;ve worked at Los Alamos National Lab and in Isaac Chuang&apos;s lab.
                 I&apos;m widely interested in quantum info theory, particularly quantum algorithms and quantum learning, and stat mech. At MIT, I&apos;m most involved in Puzzle Club,
                 Wind Ensemble, and iQuHACK.
             </p>
